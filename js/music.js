036e05e8ed8539aa1dc7278735c1d97338aab365
Jeronimo <jeronatan@hotmail.com>
2023-02-15 17:40:14 -0300

    some fixes to improve the quality of the routes and images

--- js/music.js
+++ js/music.js
@@ -2,4 +2,19 @@ let audio = document.getElementById("audio-fondo");
 
 
 audio.volume = 0.1;
-audio.play();
+
+// Crear un objeto de contexto de audio
+var audioContext = new AudioContext();
+
+// Crear un objeto de fuente de audio
+var audioSource = audioContext.createBufferSource();
+
+// Cargar el archivo de audio
+fetch('./assets/audio/song.mp3')
+  .then(response => response.arrayBuffer())
+  .then(buffer => audioContext.decodeAudioData(buffer))
+  .then(decodedData => {
+    audioSource.buffer = decodedData;
+    // Empezar a reproducir el audio
+    audioSource.start(0);
+  });
